d53c0b1b88954b3796828b070f3940b4c681982c
Alok Krishali <iamalokkrishali28@gmail.com>
2024-10-15 21:41:57 +0530

    Added Builder design pattern changes

---
+++ Assets/Script/Pattern/Builder/User.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace Assets.Script.Pattern.Builder
+{
+    public class User
+    {
+        private int UserId;
+        private string UserName;
+        private string UserType;
+        // Use this for initialization
+
+        //private User(UserBuilder uBuilder)
+        //{
+        //    this.UserId = uBuilder.
+        //}
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        static class UserBuilder
+        {
+            //private int UserId;
+            //private string UserName;
+            //private string UserType;
+
+            //public UserBuilder SetUserID()
+            //{
+
+            //}
+        }
+    }
+}
